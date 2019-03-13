83bd1846788f9b83dea14a740b26b73dcc425c59
TMCNEILL28 <48483907+TMCNEILL28@users.noreply.github.com>
2019-03-13 20:00:21 +0000

    End of Day 2

---
+++ spec/00_demo_spec.ts
@@ -0,0 +1,12 @@
+describe('Writing a Spec', () => {
+    it('is easy whe you know how!', () => {
+        //Given - Arrange
+        const a = 10, b = 20;
+        //when - Act
+        const answer = a + b;
+
+        //then - Assert
+        expect(answer).toBe(30);
+
+    });
+});
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import './styles.css';
+import { add, pi } from './utils';
+
+console.log('Ready to Party');
+
+console.log(add(10, 18));
+
+console.log(pi);
