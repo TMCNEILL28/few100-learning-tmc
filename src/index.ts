59ad79dba2a08a84051ad051587260aebdcb0407
TMCNEILL28 <48483907+TMCNEILL28@users.noreply.github.com>
2019-03-14 18:24:55 +0000

    post lunch

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,37 @@
 import './styles.css';
-import { add, pi } from './utils';
+import { getRandomInt } from './utils';
 
-console.log('Ready to Party');
+const squares = document.querySelectorAll('.square');
 
-console.log(add(10, 18));
+const secret = getRandomInt(1, 6);
+let guesses = 0;
 
-console.log(pi);
+squares.forEach((sq, idx) => {
+    const el = sq as HTMLDivElement;
+    if ((idx + 1) === secret) {
+        el.dataset['secret'] = 'true';
+    }
+    sq.addEventListener('click', function () {
+        guesses++;
+        if (el.dataset.secret) {
+            el.classList.add('winner');
+            const message = ` <small>You made <b>${guesses} </b> guesses. </small>`;
+            document.getElementById('guesses').innerHTML = message;
+        }
+        else {
+            el.classList.add('loser');
+        }
+    })
+})
+
+function handleClick() {
+    const el = this as HTMLDivElement;
+    if (el.dataset.secret) {
+        el.classList.add('winner');
+        el.removeEventListener('click', handleClick);
+    }
+    else {
+        el.classList.add('loser');
+        el.removeEventListener('click', handleClick);
+    }
+}
